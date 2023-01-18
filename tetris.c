d737379958b597b07f264410f9c09a2bd6fe286b
yalue <otternes@cs.unc.edu>
2023-01-17 20:10:55 -0500

    Start drawing game state
     - Can now draw the current falling piece, and the next piece. Also
       technically draws the board, but this isn't tested yet.
    
     - Draws the score and number of lines completed.
    
     - Writes controls to the screen.
    
     - I don't remember if those previous two points were in the prior
       commit.

--- tetris.c
+++ tetris.c
@@ -7,6 +7,11 @@
 #include <string.h>
 #include "tetris.h"
 
+// These are used for bounds checks when drawing to the screen. They account
+// for the 1 character of border and the 2-character cell width.
+#define BOARD_MAX_X (1 + (BLOCKS_WIDE * 2))
+#define BOARD_MAX_Y (1 + BLOCKS_TALL)
+
 // Calls our internal function to print the location of an error and exit if
 // a curses function returns ERR.
 #define CheckCursesError(val) InternalCheckCursesError((val), #val, __FILE__, __LINE__)
@@ -64,6 +69,17 @@ static void PrintWindowTitle(WINDOW *window, const char *s) {
   CheckCursesError(mvwprintw(window, 0, start_x, s));
 }
 
+// Prints the "Controls: " lines in the main window. Exits on error.
+static void PrintControls(WINDOW *w, int row, int col) {
+  CheckCursesError(mvwprintw(w, row, col, "Controls:"));
+  CheckCursesError(mvwprintw(w, row + 1, col, "q: quit"));
+  CheckCursesError(mvwprintw(w, row + 2, col, "l: quick load"));
+  CheckCursesError(mvwprintw(w, row + 3, col, "s: quick save"));
+  CheckCursesError(mvwprintw(w, row + 4, col, "space: pause"));
+  CheckCursesError(mvwprintw(w, row + 5, col, "arrow keys:"));
+  CheckCursesError(mvwprintw(w, row + 6, col, "  move/rotate"));
+}
+
 // Initializes the layout and empty display. Exits on error.
 static void CreateWindows(TetrisDisplay *windows) {
   int chars_wide, chars_tall, status_window_width, main_window_width;
@@ -86,6 +102,8 @@ static void CreateWindows(TetrisDisplay *windows) {
   CheckNULL(windows->top_window);
   WinBox(windows->top_window);
   PrintWindowTitle(windows->top_window, " Tetris ");
+  PrintControls(windows->top_window, chars_tall - 4, chars_wide + 5);
+
   windows->game = subwin(windows->top_window, chars_tall, chars_wide, 2, 2);
   CheckNULL(windows->game);
   WinBox(windows->game);
@@ -113,11 +131,6 @@ static void RefreshAllWindows(TetrisDisplay *windows) {
   CheckCursesError(wrefresh(windows->next_piece));
 }
 
-static void ClearGameWindow(TetrisDisplay *windows) {
-  CheckCursesError(werase(windows->game));
-  WinBox(windows->game);
-}
-
 static void DestroyWindows(TetrisDisplay *windows) {
   // We're not going to bother checking errors on these cleanup functions.
   // Technically, they return an error if any of the windows are NULL, but at
@@ -129,6 +142,164 @@ static void DestroyWindows(TetrisDisplay *windows) {
   // We must delete this last, after the sub-windows.
   delwin(windows->top_window);
   memset(windows, 0, sizeof(*windows));
+}
+
+// If c is a non-printable character (or just something we don't want to print
+// to the board, returns a ' '.  Otherwise, returns c.
+static char FixNonPrintable(char c) {
+  if ((c < ' ') || (c > '~')) return ' ';
+  return c;
+}
+
+// Takes a pointer to the game window, and the board array in the game state,
+// and draws the contents of the board.
+static void DrawBoard(WINDOW *w, uint8_t *board) {
+  // We'll use these as the coordinates in the ncurses window.
+  // The coordinates into the ncurses window.
+  int y, x;
+  // The index into the board array.
+  int i = 0;
+  uint8_t c;
+  // Note that we start at y = 1 and x = 1 to skip the window border.
+  for (y = 1; y <= BLOCKS_TALL; y++) {
+    for (x = 1; x <= (BLOCKS_WIDE * 2); x += 2) {
+      c = FixNonPrintable(board[i]);
+      // NOTE: Replace with something more efficient than format strings.
+      CheckCursesError(mvwprintw(w, y, x, "%c%c", c, c));
+      i++;
+    }
+  }
+}
+
+// Takes a pointer to the next piece window, and the index of the piece in the
+// tetris_pieces array. Draws the piece in the window.
+static void DrawNextPiece(WINDOW *w, uint8_t piece) {
+  const char *p = tetris_pieces[piece];
+  // Follows a very similar pattern to DrawBoard
+  int x, y, i = 0;
+  char c;
+  for (y = 2; y < 6; y++) {
+    for (x = 2; x < 10; x += 2) {
+      c = p[i];
+      CheckCursesError(mvwprintw(w, y, x, "%c%c", c, c));
+      i++;
+    }
+  }
+}
+
+// Returns 1 if a space is available for a falling block, and 0 if not. Returns
+// 0 if the given position is either out of bounds, or overlapping with an
+// existing block. The x, y coordinate is in game cells, NOT ncurses rows/cols.
+static int SpaceAvailable(uint8_t *board, int x, int y) {
+  char c;
+  if ((x < 0) || (y < 0)) return 0;
+  if ((x >= BLOCKS_WIDE) || (y >= BLOCKS_TALL)) return 0;
+  c = board[y * BLOCKS_WIDE + x];
+  return (c <= ' ') || (c >= '~');
+}
+
+// TODO: Use SpaceAvailable when moving pieces, either side to side, rotation,
+// or falling.
+
+static void DrawFallingPiece(WINDOW *w, TetrisGameState *s) {
+  // TODO: Need DrawFallingPiece.
+  //  - If partially above the top of the screen, just don't draw what's above.
+  const char *p = tetris_pieces[s->current_piece];
+  int x, y, screen_x, init_screen_x, i = 0;
+  char c;
+  screen_x = (2 * s->piece_x) + 1;
+  init_screen_x = screen_x;
+  for (y = s->piece_y; y < (s->piece_y + 4); y++) {
+    for (x = s->piece_x; x < (s->piece_x + 4); x++) {
+      c = p[i];
+      // We ignore empty parts of the piece, as we wouldn't want to overwrite
+      // existing blocks with empty space.
+      if (c == ' ') {
+        i++;
+        screen_x += 2;
+        continue;
+      }
+      // Since we're dealing with a part of the block that's actually there,
+      // ensure that it's within the board boundaries and not overlapping any
+      // existing piece.
+      if (!SpaceAvailable(s->board, x, y)) {
+        endwin();
+        printf("The current piece is in an invalid location!\n");
+        exit(1);
+      }
+      CheckCursesError(mvwprintw(w, y + 1, screen_x, "%c%c", c, c));
+      screen_x += 2;
+      i++;
+    }
+    screen_x = init_screen_x;
+  }
+}
+
+// TODO: Sanity checks for quickload:
+//  - Make sure there are no completed rows in the board (would be impossible
+//    in a quicksave).
+//  - Score > lines
+//  - lines not negative
+//  - Next piece a valid index.
+//  - Current piece a valid index.
+//  - piece_x and piece_y are valid.
+
+// Writes the score, piece, and so on, in the game window.
+static void DisplayGameState(TetrisDisplay *windows, TetrisGameState *s) {
+  DrawBoard(windows->game, s->board);
+  DrawNextPiece(windows->next_piece, s->next_piece);
+  DrawFallingPiece(windows->game, s);
+  CheckCursesError(mvwprintw(windows->score, 1, 2, "%011d", s->score));
+  CheckCursesError(mvwprintw(windows->line_count, 1, 2, "%011d", s->lines));
+}
+
+// Returns a random piece to drop down (i.e., an index into tetris_pieces).
+static uint8_t RandomNewPiece(void) {
+  // Since some pieces have up to four rotations, this allows us to select a
+  // random piece rotation without weighting pieces with more rotations over
+  // pieces with fewer, as every piece has four entries.
+  static const uint8_t piece_ids[] = {0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4,
+    5, 5, 6, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
+  const uint8_t max_choice = sizeof(piece_ids) / sizeof(uint8_t);
+  return piece_ids[rand() % max_choice];
+}
+
+// Attempts to quickload a game state from the quicksave file. If any error
+// occurs in loading or validating the file, this will *not* exit or crash;
+// instead it will just print an error message to the game display and return
+// 0. If any error occurs, this will *not* modify s.
+static int TryQuickload(TetrisDisplay *windows, TetrisGameState *s) {
+  // TODO: Implement TryQuickload.
+  return 0;
+}
+
+// Sets up a new game, clearing the board, setting score and lines to 0, and
+// setting up the current and next piece.
+static void InitializeNewGame(TetrisGameState *s) {
+  memset(s, 0, sizeof(*s));
+  memset(s->board, ' ', sizeof(s->board));
+  s->next_piece = RandomNewPiece();
+  s->current_piece = RandomNewPiece();
+  // The piece starts at the top, in the middle.
+  s->piece_y = 14; //////////////////////////////////////////// DEBUG
+  s->piece_x = BLOCKS_WIDE / 2;
+}
+
+// Runs a game until a quit or a game over occurs. Returns 0 on a game over,
+// and nonzero on a quit. The initial_quickload argument should be zero to
+// start a new game, and nonzero if we should attempt to load a quicksave
+// immediately.
+static int RunGame(TetrisDisplay *windows, int initial_quickload) {
+  TetrisGameState s;
+  if (initial_quickload) {
+    if (!TryQuickload(windows, &s)) {
+      InitializeNewGame(&s);
+    }
+  } else {
+    InitializeNewGame(&s);
+  }
+  DisplayGameState(windows, &s);
+  return 0;
 }
 
 int main(int argc, char **argv) {
@@ -144,21 +315,18 @@ int main(int argc, char **argv) {
   // start.
   should_exit = 0;
   while (!should_exit) {
-    ClearGameWindow(&windows);
     CheckCursesError(mvwprintw(windows.game, 9, 6, "Press space"));
     CheckCursesError(mvwprintw(windows.game, 10, 7, "to start!"));
     RefreshAllWindows(&windows);
-    // TODO (next):
-    //  - Clear all window centers
-    //  - Write "Press space to start"
-    //  - Write "Press q to quit"
     input_key = getch();
     switch (input_key) {
     case (' '):
-      addstr("Space pressed\n");
-      // TODO: Run the game here!
-      //  - Draw the game window on the new game screen with "press space to start"
-      //    in the middle.
+      should_exit = RunGame(&windows, 0);
+      break;
+    case ('l'):
+      // Here, we just call RunGame like normal, except instruct it to
+      // immediately try loading the quicksave if it exists.
+      should_exit = RunGame(&windows, 1);
       break;
     case ('q'):
       should_exit = 1;
@@ -169,5 +337,6 @@ int main(int argc, char **argv) {
   }
   DestroyWindows(&windows);
   endwin();
+  printf("Tetris exited normally!\n");
   return 0;
 }
--- tetris.h
+++ tetris.h
@@ -34,27 +34,135 @@ typedef struct {
   // Holds the state of the entire board. If a cell is empty, it must be either
   // 0 or a space character. In any other case, the cell is occupied, and this
   // must contain the character to draw to the screen when drawing the cell.
-  // (Note that cells are drawn as two characters wide, so each nonzero
-  // character in this array will be printed twice when drawing the board.)
-  char board[BLOCKS_WIDE * BLOCKS_TALL];
-
-  // The ID of the next piece that will be generated.
-  char next_piece;
-
+  // Note that cells are drawn as two characters wide, so each character in
+  // this array will be printed twice when drawing the board. This starts from
+  // the TOP row in the window that's displayed.
+  uint8_t board[BLOCKS_WIDE * BLOCKS_TALL];
+  // The ID of the next piece that will be generated; the index into the
+  // tetris_pieces array.
+  uint8_t next_piece;
   // The x and y location of the current piece being dropped into the board, in
-  // a cell coordinate rather than a window character.
-  int piece_x;
-  int piece_y;
-
-  // The piece that is currently "falling".
-  char current_piece;
-
+  // a cell coordinate rather than a window character. Note that the coordinate
+  // of a piece corresponds to the top left corner of its definition in the
+  // tetris_pieces array.
+  int32_t piece_x;
+  int32_t piece_y;
+  // The piece that is currently "falling". Once again, it's an index into the
+  // tetris_pieces array.
+  uint8_t current_piece;
   // The player's current score.
-  int score;
-
+  int32_t score;
   // The number of lines the player has completed.
-  int lines;
+  int32_t lines;
 } TetrisGameState;
+
+// We statically define each piece as a 16-byte strings here. Note that when
+// the game is being rendered, the widths of these will be doubled. This
+// contains all pieces along with their rotations. There are 19 of them.
+const char *tetris_pieces[] = {
+  // 0:
+  "===="
+  "    "
+  "    "
+  "    ",
+  // 1:
+  "=   "
+  "=   "
+  "=   "
+  "=   ",
+  // 2:
+  "HH  "
+  "HH  "
+  "    "
+  "    ",
+  // 3:
+  "N   "
+  "NN  "
+  " N  "
+  "    ",
+  // 4:
+  " NN "
+  "NN  "
+  "    "
+  "    ",
+  // 5:
+  " Z  "
+  "ZZ  "
+  "Z   "
+  "    ",
+  // 6:
+  "ZZ  "
+  " ZZ "
+  "    "
+  "    ",
+  // 7:
+  " #  "
+  "### "
+  "    "
+  "    ",
+  // 8:
+  "#   "
+  "##  "
+  "#   "
+  "    ",
+  // 9:
+  "### "
+  " #  "
+  "    "
+  "    ",
+  // 10:
+  " #  "
+  "##  "
+  " #  "
+  "    ",
+  // 11:
+  "@   "
+  "@   "
+  "@@  "
+  "    ",
+  // 12:
+  "@@@ "
+  "@   "
+  "    "
+  "    ",
+  // 13:
+  "@@  "
+  " @  "
+  " @  "
+  "    ",
+  // 14:
+  "  @ "
+  "@@@ "
+  "    "
+  "    ",
+  // 15:
+  " O  "
+  " O  "
+  "OO  "
+  "    ",
+  // 16:
+  "O   "
+  "OOO "
+  "    "
+  "    ",
+  // 17:
+  "OO  "
+  "O   "
+  "O   "
+  "    ",
+  // 18:
+  "OOO "
+  "  O "
+  "    "
+  "    ",
+  NULL,
+};
+
+// This is how we look up rotations. If the current piece is at index i in the
+// tetris_pieces array, then piece_rotations[i] gives the index of its next
+// rotation in the tetris_pieces array.
+uint8_t piece_rotations[] = {1, 0, 2, 4, 3, 6, 5, 8, 9, 10, 7, 12, 13, 14, 11,
+  16, 17, 18, 15};
 
 #endif  // TETRIS_H
 
